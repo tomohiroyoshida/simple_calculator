5aeb4f6bdd021cd535cc7372dc805b3c507d6682
tomohiroyoshida <tomohiroyoshida0511@gmail.com>
2020-05-11 18:39:20 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
+    <hr>
+    <p>{{result}}</P>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data:function () {
+    return {
+      message: 'HELLO',
+      result: 'no-event',
+    };
+  },
+  methods: {
+    appAction: function (message) {
+      this.result =  '(*** You send:"' + message + '". ***)';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
